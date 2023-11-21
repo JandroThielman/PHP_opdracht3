3f166758f585e23321ad48e49264857b13644f8a
JandroThielman <9020498@student.zadkine.nl>
2023-11-21 11:49:40 +0100

    jandro

---
+++ Index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Style.css">
+    <title>Opdracht3</title>
+</head>
+<body>
+    <?php
+    $jandro = "Jandro is cool 😎🫠🥸🤖👨🏽‍💻";
+    $age1 = "10";
+    $age2 = "7";
+    $tot = $age1 + $age2;
+
+    echo "<h1>$jandro</h1>";
+    echo "<br>";
+    echo "<h1>Ik ben $tot Jaar oud.</h1>";
+    ?>
+</body>
+</html>
